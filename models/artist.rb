d93bd7cb5143230a03bbc1206aa9debd2ef6e1d6
Matteo Fusillo <matt.fusillo@gmail.com>
2019-12-05 10:23:22 +0000

    refactored if statement in self.find method

--- models/artist.rb
+++ models/artist.rb
@@ -4,68 +4,68 @@ require_relative('album')
 
 class Artist
 
-attr_reader :id
-attr_accessor :name
+  attr_reader :id
+  attr_accessor :name
 
   def initialize(options)
     @id = options['id'] if options['id']
     @name = options['name']
   end
 
-def save()
+  def save()
     sql = "INSERT INTO artists
-      (
+    (
       name
       ) VALUES
       (
-      $1
-      )  RETURNING id;"
-    values = [@name]
-    @id = SqlRunner.run(sql, values)[0]['id'].to_i
-end
+        $1
+        )  RETURNING id;"
+        values = [@name]
+        @id = SqlRunner.run(sql, values)[0]['id'].to_i
+      end
 
-def self.delete_all()
-  sql = "DELETE FROM artists"
-  SqlRunner.run(sql)
-end
+      def self.delete_all()
+        sql = "DELETE FROM artists"
+        SqlRunner.run(sql)
+      end
 
-def delete()
-  sql = "DELETE FROM artists
-  WHERE id = $1;"
-  values = [@id]
-  SqlRunner.run(sql, values)
-end
+      def delete()
+        sql = "DELETE FROM artists
+        WHERE id = $1;"
+        values = [@id]
+        SqlRunner.run(sql, values)
+      end
 
-def self.all()
-  sql = "SELECT * FROM artists;"
-  results = SqlRunner.run(sql)
-  artists = results.map {|artist| Artist.new(artist)}
-  return artists
-end
+      def self.all()
+        sql = "SELECT * FROM artists;"
+        results = SqlRunner.run(sql)
+        artists = results.map {|artist| Artist.new(artist)}
+        return artists
+      end
 
-def albums()
-  sql = "SELECT * FROM albums WHERE artist_id = $1;"
-  values = [@id]
-  results = SqlRunner.run(sql, values)
-  albums = results.map {|album| Album.new(album)}
-  return albums
-end
+      def albums()
+        sql = "SELECT * FROM albums WHERE artist_id = $1;"
+        values = [@id]
+        results = SqlRunner.run(sql, values)
+        albums = results.map {|album| Album.new(album)}
+        return albums
+      end
 
-def update()
-  sql = "UPDATE artists SET
-  name = $1
-  WHERE id = $2;"
-  values = [@name, @id]
-  SqlRunner.run(sql, values)
-end
+      def update()
+        sql = "UPDATE artists SET
+        name = $1
+        WHERE id = $2;"
+        values = [@name, @id]
+        SqlRunner.run(sql, values)
+      end
 
-def self.find(id)
-  sql = "SELECT * FROM artists WHERE id = $1;"
-  values = [id]
-  results = SqlRunner.run(sql, values)
-  return nil if results[0] == nil
-  artist_hash = results[0]
-  artist = Artist.new(artist_hash)
-  return artist
-end
-end
+      def self.find(id)
+        sql = "SELECT * FROM artists WHERE id = $1;"
+        values = [id]
+        results = SqlRunner.run(sql, values)
+        return nil if results.first == nil
+        artist_hash = results.first
+        artist = Artist.new(artist_hash)
+        return artist
+      end
+    end
